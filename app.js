505cf995523ef82de3b6cc80322d7ef3e977b5f0
Chris Laughter <chrislaugh@gmail.com>
2020-04-03 10:03:50 -0600

    scripting

---
+++ app.js
@@ -0,0 +1,20 @@
+// Listen for results
+document
+  .getElementById("Loan-form")
+  .addEventListener("submit", calculateResults);
+
+// Calculate results
+function calculateResults(e) {
+  console.log("Calculating...");
+  // UI Vars
+  const amount = document.getElementById("amount");
+  const interest = document.getElementById("interest");
+  const years = document.getElementById("years");
+  const monthlyPayment = document.getElementById("monthly-payment");
+  const totalPayment = document.getElementById("total-payment");
+  const totalInterest = document.getElementById("total-interest");
+
+  const principal = parseFloat(amount.value);
+  const calculate = parseFloat(Interest.value) / 100 / 12;
+  e.preventDefault();
+}
